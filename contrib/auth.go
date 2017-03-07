b258f036cf93a2fa9f9e11b7fdec2b7261924b26
longxboy <longxia.cao@daocloud.io>
2017-03-07 22:41:09 +0800

    add daocloud.io support

--- contrib/auth.go
+++ contrib/auth.go
@@ -1,9 +1,37 @@
 package contrib
 
+import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+var daoUrl *url.URL
+
 func InitAuth(authUrl string) error {
+	if authUrl == "" {
+		return fmt.Errorf("auth url not be empty")
+	}
+	var err error
+	daoUrl, err = url.Parse(authUrl)
+	if err != nil {
+		return false, fmt.Errorf("DAOKEEPER_URL Parse error %s,%v", daokeeper, err)
+	}
+	daoUrl.Path = "/v1/ngrokd/auth"
 	return nil
 }
 
 func Auth(authToken string) (bool, error) {
+	resp, err := http.PostForm(u.String(), url.Values{"user": {authToken}})
+	if err != nil {
+		return false, fmt.Errorf("Request daokeeper error %s,%v", u.String(), err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 {
+		return true, nil
+	} else {
+		return false, fmt.Errorf("Response daokeeper code %d,%v", resp.StatusCode)
+	}
 	return true, nil
 }
--- contrib/notify.go
+++ contrib/notify.go
@@ -1,13 +1,43 @@
 package contrib
 
+import (
+	log "github.com/Sirupsen/logrus"
+	"gopkg.in/redis.v5"
+)
+
+var redisCli *redis.Client
+
 func InitNotify(notifyUrl string) error {
+	if notifyUrl == "" {
+		log.Fatalln("redisAddr is empty")
+	}
+	redisCli = redis.NewClient(&redis.Options{
+		Addr:     notifyUrl,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	pong, err := redisCli.Ping().Result()
+	if err != nil {
+		log.WithFields(log.Fields{"err": err}).Errorln("redis ping failed!")
+		return err
+	}
+	log.WithFields(log.Fields{"pong": pong}).Infoln("init Notify,ping redis success!")
 	return nil
 }
 
 func AddMember(domain string, member string) error {
+	cmd := redisCli.SAdd(domain, member)
+	if cmd.Err() != nil {
+		return cmd.Err()
+	}
 	return nil
 }
 
 func RemoveMember(domain string, member string) error {
+	cmd := redisCli.SRem(domain, member)
+	if cmd.Err() != nil {
+		return cmd.Err()
+	}
 	return nil
 }
